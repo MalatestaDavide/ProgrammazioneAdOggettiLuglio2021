faa7d936cec80084b69f55873ea0b1012e467ad3
MalatestaDavide <davide.malatesta99@gmail.com>
2021-07-06 17:38:29 +0200

    prova

---
+++ FootballApp/FootballApp/src/test/java/it/univpm/FootballApp/FootballAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.FootballApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
